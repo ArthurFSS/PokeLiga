51b86742ecef34a5f428239e3113fab6ffbb231a
Arthur <atwiiister@gmail.com>
2024-05-24 12:54:20 -0300

    ajuste no grid

--- src/pages/TableGlc.jsx
+++ src/pages/TableGlc.jsx
@@ -1,41 +1,43 @@
 import React from 'react';
-import { Box, Container } from '@material-ui/core';
+import { Box, Container, Grid } from '@material-ui/core';
 import PlayerCard from '../components/PlayerCard';
 
 const players = [
-    {
-      name: 'Andrew Dias (Dados Fake)',
-      lastVictoryDate: '2024-05-24',
-      imageUrl: 'url-to-image-1',
-      statuses: [true, false, true, false, true, false, true, false, true],
-    },
-    {
-      name: 'Arthur Angelo (Dados Fake)',
-      lastVictoryDate: '2024-05-22',
-      imageUrl: 'url-to-image-2',
-      statuses: [false, true, false, true, false, true, false, true, false],
-    },
-    {
-        name: 'Cramorant (Dados Fake)',
-        lastVictoryDate: '2024-05-22',
-        imageUrl: 'url-to-image-2',
-        statuses: [true, true, true, true, true, true, true, true, true],
-      },
-    // Adicione mais jogadores conforme necessário
-  ];
-  
-  const PlayerList = () => {
-    return (
-      <Container style={{ display: 'flex', justifyContent: 'center'}}>
-        <Box display="flex" flexDirection="column" mt={2} width="80%" >
+  {
+    name: 'Andrew Dias (Dados Fake)',
+    lastVictoryDate: '2024-05-24',
+    imageUrl: 'url-to-image-1',
+    statuses: [true, false, true, false, true, false, true, false, true],
+  },
+  {
+    name: 'Arthur Angelo (Dados Fake)',
+    lastVictoryDate: '2024-05-22',
+    imageUrl: 'url-to-image-2',
+    statuses: [false, true, false, true, false, true, false, true, false],
+  },
+  {
+    name: 'Cramorant (Dados Fake)',
+    lastVictoryDate: '2024-05-22',
+    imageUrl: 'url-to-image-2',
+    statuses: [true, true, true, true, true, true, true, true, true],
+  },
+  // Adicione mais jogadores conforme necessário
+];
+
+const PlayerList = () => {
+  return (
+    <Container>
+      <Box display="flex" flexDirection="column" alignItems="center" mt={2}>
+        <Grid container spacing={2} justifyContent="center">
           {players.map((player, index) => (
-            <Box key={index} mb={2} mt={index === 0 ? 2 : 0} width="100%">
+            <Grid item key={index}>
               <PlayerCard {...player} />
-            </Box>
+            </Grid>
           ))}
-        </Box>
-      </Container>
-    );
-  };
-  
-  export default PlayerList;
+        </Grid>
+      </Box>
+    </Container>
+  );
+};
+
+export default PlayerList;
